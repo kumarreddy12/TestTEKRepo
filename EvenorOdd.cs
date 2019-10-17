5a57b968266425bd7b9d9891e8c746c8b1a6c639
kumarreddy12 <56688601+kumarreddy12@users.noreply.github.com>
2019-10-17 14:28:31 -0400

    Done changes

--- EvenorOdd.cs
+++ EvenorOdd.cs
@@ -29,5 +29,5 @@ namespace check1
     }
 }
 /*
- * C# Program to Check whether the Entered Number is Even or Odd test
+ * C# Program to Check whether the Entered Number is Even or Odd test done
  */
